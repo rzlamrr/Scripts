4c39949f2e8530bd937ac68aab1de6a0d180f973
rzlamrr <rizal82rebel@gmail.com>
2020-12-29 13:09:54 +0700

    new script

--- KERNEL/build.sh
+++ KERNEL/build.sh
@@ -5,14 +5,14 @@
 
 if [[ -n $CI ]] ; then
     echo "Yeay, build running on CI!" ;
-        if [[ -z $chat_id ]] && [[ -z $token ]] ; then
+        if [[ -z $CHATID ]] && [[ -z $TOKED ]] ; then
             echo 'chat id and bot token is not set or empty.' ;
             exit 1 ;
         fi
     ls -Aq &>/dev/null
 else
     echo "Okay, build running on VM!" ;
-        if [[ -z $chat_id ]] && [[ -z $token ]] ; then
+        if [[ -z $CHATID ]] && [[ -z $TOKED ]] ; then
             read -p "Enter your chat id: " chat_id
             read -p "Enter your bot token: " token
             export chat_id token
@@ -21,11 +21,8 @@ else
 fi
 
 config_path="$(pwd)/arch/arm64/configs"
-if [[ -e "$config_path/ugglite_defconfig" ]] ; then
-    device=ugglite && config_device1=ugglite_defconfig
-elif [[ ( -e "$config_path/rolex_defconfig" || -e "$config_path/riva_defconfig" ) ]] ; then
-    device=rova && config_device1=rolex_defconfig && config_device2=riva_defconfig
-fi
+device=riva
+config_device=riva_defconfig
 
 [[ ! -d "$(pwd)/anykernel-3" ]] && git clone https://github.com/fadlyas07/anykernel-3 --depth=1 &>/dev/null
 [[ ! -d "$(pwd)/origin_gcc" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 -b android-9.0.0_r59 origin_gcc &>/dev/null
@@ -34,10 +31,10 @@ fi
 # Needed to export
 export ARCH=arm64
 export SUBARCH=arm64
-export TELEGRAM_ID=$chat_id
-export TELEGRAM_TOKEN=$token
-export KBUILD_BUILD_USER=fadlyas07
-export KBUILD_BUILD_HOST=circleci-Lab
+export TELEGRAM_ID=$CHATID
+export TELEGRAM_TOKEN=$TOKED
+export KBUILD_BUILD_USER=rzlamrr
+export KBUILD_BUILD_HOST=dvstLab
 
 mkdir "$(pwd)/temporary"
 
@@ -62,7 +59,7 @@ build_kernel() {
 }
 
 # Main Environment
-product_name='GreenForce'
+product_name='Axylon'
 temp="$(pwd)/temporary"
 pack="$(pwd)/anykernel-3"
 kernel_img="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
@@ -70,46 +67,29 @@ kernel_img="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
 build_start=$(date +"%s")
 
 # build kernel - 1
-build_date1="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
-make ARCH=arm64 O=out $config_device1 &>/dev/null
+build_date="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
+make ARCH=arm64 O=out $config_device &>/dev/null
 build_kernel 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
 mv Log-*.log $temp
 
 if [[ ! -f "$kernel_img" ]] ; then
     build_end=$(date +"%s")
     build_diff=$(($build_end - $build_start))
-    curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
+    curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
     tg_send_message "<b>build throw an errors!</b> ($(git rev-parse --abbrev-ref HEAD), Build took $(($build_diff / 60)) minutes, $(($build_diff % 60)) seconds."
     exit 1 ;
 fi
 
+TEMPZIPNAME=$product_name-RIVA-"$build_date"-unsigned.zip
+ZIPNAME=$product_name-RIVA-"$build_date".zip
 kernel_version="$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)"
-curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
-mv "$kernel_img" "$pack/zImage" && cd $pack || exit 1 ;
-if [[ "$device" == "ugglite" ]] ; then
-    zip -r9 $product_name-ugglite-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd .. || exit 1 ;
-    curl -F chat_id="$TELEGRAM_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@"$(echo $pack/*.zip)" "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F caption="
-    New #ugglite build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo $pack/*.zip)" | awk '{ print $1 }')."
-elif [[ "$device" == "rova" ]] ; then
-    zip -r9 $product_name-rolex-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd .. || exit 1 ;
-    curl -F chat_id="$TELEGRAM_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@"$(echo $pack/*.zip)" "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F caption="
-    New #rolex build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo $pack/*.zip)" | awk '{ print $1 }')."
-fi
-
-if [[ "$device" != "ugglite" ]] ; then
-    rm -rf out $pack/*.zip $temp/Log-*.log $pack/zImage
-
-    # build kernel - 2
-    build_date2="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
-    make ARCH=arm64 O=out $config_device2 &>/dev/null
-    build_kernel 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
-    mv Log-*.log $temp
-
-    curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
-    mv "$kernel_img" "$pack/zImage" && cd $pack || exit 1 ;
-    if [[ "$device" == "rova" ]] ; then
-        zip -r9 $product_name-riva-"$build_date2".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd .. || exit 1 ;
-        curl -F chat_id="$TELEGRAM_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@"$(echo $pack/*.zip)" "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F caption="
-        New #riva build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo $pack/*.zip)" | awk '{ print $1 }')."
-    fi
-fi
+curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
+mv "$kernel_img" "$pack/zImage"
+cd $pack
+zip -r9 $TEMPZIPNAME * -x .git README.md LICENCE $(echo *.zip) &>/dev/null
+curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
+java -jar zipsigner-3.0.jar "${TEMPZIPNAME}" "${ZIPNAME}"
+rm $TEMPZIPNAME
+cd ..
+curl -F chat_id="$TELEGRAM_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@"$(echo "$pack"/*RIVA*.zip)" "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F caption="New #riva build is available!
+($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo "$pack"/*.zip)" | awk '{ print $1 }')."
