62a8ab8cf1302192676ad10ebadeea2b8eb373e5
rzlamrr <rizal82rebel@gmail.com>
2021-04-15 21:25:54 +0000

    KERNEL: caf-driver: automate commit

--- KERNEL/caf-driver.sh
+++ KERNEL/caf-driver.sh
@@ -12,13 +12,13 @@ audio="https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-ke
 data="https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel/"
 
 merge() {
-if [[ "${INITIAL}" =~ "true" ]]; then
+if [[ "${INITIAL}" == "true" ]]; then
     #just for initial merge
-    /usr/lib/git-core/git-subtree add --prefix drivers/staging/qcacld-3.0 "${qcacld}" "${TAG}"
-    /usr/lib/git-core/git-subtree add --prefix drivers/staging/fw-api "${fw_api}" "${TAG}"
-    /usr/lib/git-core/git-subtree add --prefix drivers/staging/qca-wifi-host-cmn "${qca_wifi_host_cmn}" "${TAG}"
-    /usr/lib/git-core/git-subtree add --prefix techpack/audio "${audio}" "${TAG}"
-    /usr/lib/git-core/git-subtree add --prefix techpack/data "${data}" "${TAG}"
+    git subtree add --prefix drivers/staging/qcacld-3.0 "${qcacld}" "${TAG}"
+    git subtree add --prefix drivers/staging/fw-api "${fw_api}" "${TAG}"
+    git subtree add --prefix drivers/staging/qca-wifi-host-cmn "${qca_wifi_host_cmn}" "${TAG}"
+    git subtree add --prefix techpack/audio "${audio}" "${TAG}"
+    git subtree add --prefix techpack/data "${data}" "${TAG}"
 elif [[ "${INITIAL}" =~ "merge" ]]; then
     git fetch "${qcacld}" "${TAG}"
     git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD && git read-tree --prefix=drivers/staging/qcacld-3.0 -u FETCH_HEAD
@@ -36,11 +36,16 @@ elif [[ "${INITIAL}" =~ "merge" ]]; then
     git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD && git read-tree --prefix=techpack/data -u FETCH_HEAD
     git commit -m "techpack/data: Initial merge from ${TAG}"
 else
-    git fetch "${qcacld}" "${TAG}" && git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
-    git fetch "${fw_api}" "${TAG}" && git merge -X subtree=drivers/staging/fw-api FETCH_HEAD
-    git fetch "${qca_wifi_host_cmn}" "${TAG}" && git merge -X subtree=drivers/staging/qca_wifi_host_cmn FETCH_HEAD
-    git fetch "${audio}" "${TAG}" && git merge -X subtree=techpack/audio FETCH_HEAD
-    git fetch "${data}" "${TAG}" && git merge -X subtree=techpack/data FETCH_HEAD
+    git fetch "${qcacld}" "${TAG}" && git merge --no-commit -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
+    git commit -m "qcacld: Merge tag ${TAG}"
+    git fetch "${fw_api}" "${TAG}" && git merge --no-commit -X subtree=drivers/staging/fw-api FETCH_HEAD
+    git commit -m "fw-api: Merge tag ${TAG}"
+    git fetch "${qca_wifi_host_cmn}" "${TAG}" && git merge --no-commit -X subtree=drivers/staging/qca_wifi_host_cmn FETCH_HEAD
+    git commit -m "qca-wifi-host-cmn: Merge tag ${TAG}"
+    git fetch "${audio}" "${TAG}" && git merge --no-commit -X subtree=techpack/audio FETCH_HEAD
+    git commit -m "techpack/audio: Merge tag ${TAG}"
+    git fetch "${data}" "${TAG}" && git merge --no-commit -X subtree=techpack/data FETCH_HEAD
+    git commit -m "techpack/data: Merge tag ${TAG}"
     fi
 }
 
